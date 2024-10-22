99d03f24aa60a3c6a8cfc3677c68252244d93e94
enesJav16 <maratuenes@gmail.com>
2024-10-22 23:06:04 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Programmer programmer=new Programmer("Enes","Junior","PeakSoft");
+        Dancer dancer =new Dancer("SAid","Experienced","Fixers");
+        Singer singer=new Singer("Ensa","Super star","Super Stars");
+
+
+        System.out.println(programmer.toString());
+        System.out.println("================================");
+        System.out.println(dancer.toString());
+        System.out.println("================================");
+        System.out.println(singer.toString());
+
     }
 }
